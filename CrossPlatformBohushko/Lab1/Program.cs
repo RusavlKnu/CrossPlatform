7b792bd293840059f140e47438db52b6c0e8888f
Rusavladev <rusavladev@gmail.com>
2023-10-21 13:29:51 +0300

    Added first 3 labs

---
+++ CrossPlatformBohushko/Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int N;
+        using (StreamReader reader = new StreamReader("..//..//..//INPUT.txt"))
+        {
+            string line = reader.ReadLine();
+            string nextLine = reader.ReadLine();
+
+            if (nextLine != null || !int.TryParse(line, out N))
+            {
+                WriteError("Error: INPUT.txt should contain a single integer value.");
+                return;
+            }
+        }
+
+        if (N < 1 || N > 109)
+        {
+            WriteError("Error: Invalid value of N. It should be in the range 1 ≤ N ≤ 109.");
+            return;
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
+            {
+                count++;
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter("..//..//..//OUTPUT.txt"))
+        {
+            writer.WriteLine(count);
+        }
+    }
+
+    static void WriteError(string errorMessage)
+    {
+        using (StreamWriter writer = new StreamWriter("..//..//..//OUTPUT.txt"))
+        {
+            writer.WriteLine(errorMessage);
+        }
+    }
+}
